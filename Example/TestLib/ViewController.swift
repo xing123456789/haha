4f6385b4dfcf58a1fefb9a681371a0257ca347b9
xing123456789 <95727133+xing123456789@users.noreply.github.com>
2022-08-03 18:03:12 +0800

    af

--- Example/TestLib/ViewController.swift
+++ Example/TestLib/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import TestLib
+import AFNetworking
 
 class ViewController: UIViewController {
 
@@ -17,6 +18,8 @@ class ViewController: UIViewController {
         let webview = CustomWebView(frame: CGRect(x: 0, y: 100, width: 300, height: 500))
         webview.openWebView(html: "http://www.baidu.com")
         self.view.addSubview(webview)
+        
+
     }
 
     override func didReceiveMemoryWarning() {
